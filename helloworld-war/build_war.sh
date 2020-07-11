e7ae2d6653e691e38ff0b1a956db82a60255d903
gyang <gongbi.yang@zuora.com>
2020-07-12 01:55:18 +0800

    update build_war.sh

--- helloworld-war/build_war.sh
+++ helloworld-war/build_war.sh
@@ -3,5 +3,5 @@
 docker container run \
     --volume $(pwd)/war_builder/scripts:/scripts \
     --volume $(pwd)/webapps:/workdir \
-    --rm -it --name webapp_builder war_builder:v1 \
+    --rm --name webapp_builder war_builder:v1 \
     build

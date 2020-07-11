619af2d7302590b33af6272f18e1459536460d30
gyang <gongbi.yang@zuora.com>
2020-07-11 21:29:42 +0800

    remove sudo

--- helloworld-war/build_war.sh
+++ helloworld-war/build_war.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo docker container run \
+docker container run \
     --volume $(pwd)/war_builder/scripts:/scripts \
     --volume $(pwd)/webapps:/workdir \
     --rm -it --name webapp_builder war_builder:v1 \
--- helloworld-war/war_builder/build_war_builder.sh
+++ helloworld-war/war_builder/build_war_builder.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo docker image build \
+docker image build \
     --file war_builder.dockerfile \
     --tag war_builder:v1 .
 
-sudo docker image ls
+docker image ls
